b67da34518d4e3cce88332e8e7ed0f4c2328fb8c
WantedHool <christarasrib@gmail.com>
2022-06-18 13:12:29 +0300

    Add project files.

---
+++ DapperContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EducationalSoftwareServer
+{
+    public class DapperContext
+    {
+        private string _connectionString = "Data Source=DESKTOP-6HR71TI;Initial Catalog=EducationalSoftware;Integrated Security=True";
+        public DapperContext(/*IConfiguration configuration*/)
+        {
+            //OnConfiguring();
+            //_configuration = configuration;
+            //_connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+        //protected void OnConfiguring()
+        //{
+        //    IConfigurationRoot configuration = new ConfigurationBuilder()
+        //        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+        //    _connectionString = configuration.GetConnectionString("DefaultConnection");
+        //}
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace EducationalSoftwareServer
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserType UserType { get; set; }
+
+    }
+}
