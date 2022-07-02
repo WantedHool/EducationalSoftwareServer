9409f1f0016b4f14343efb164918ba46174aa927
NfskM <nafsika.m25@gmail.com>
2022-07-02 13:33:15 +0300

    katiiii

--- Client/EducationalSoftwareClient/src/app/app-routing.module.ts
+++ Client/EducationalSoftwareClient/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GradesComponent } from './grades/grades.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { TestsComponent } from './tests/tests.component';
+import { TheoryComponent } from './theory/theory.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -11,13 +14,13 @@ const routes: Routes = [
   { path: 'register/Student', component: RegisterComponent},
   { path: 'home', component: HomeComponent},
   { path: 'home/Teacher', component: HomeComponent},
-  { path: 'home/Teacher/grades', component: HomeComponent},
-  { path: 'home/Teacher/tests', component: HomeComponent},
-  { path: 'home/Teacher/theory', component: HomeComponent},
+  { path: 'home/Teacher/grades', component: GradesComponent},
+  { path: 'home/Teacher/tests', component: TestsComponent},
+  { path: 'home/Teacher/theory', component: TheoryComponent},
   { path: 'home/Student', component: HomeComponent},
-  { path: 'home/Student/Grades', component: HomeComponent},
-  { path: 'home/Student/Tests', component: HomeComponent},
-  { path: 'home/Student/Theory', component: HomeComponent}
+  { path: 'home/Student/Grades', component: GradesComponent},
+  { path: 'home/Student/Tests', component: TestsComponent},
+  { path: 'home/Student/Theory', component: TheoryComponent}
 
 ];
 
